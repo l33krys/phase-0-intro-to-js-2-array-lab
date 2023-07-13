7e405fc4ad5c0b0d1087fe1f382540ada5cdfc4d
Krystle Lee <l33.krys@gmail.com>
2023-07-12 18:02:31 -0700

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name) {
+    return cats.push(name);
+}
+
+function destructivelyPrependCat(name) {
+    return cats.unshift(name);
+}
+
+function destructivelyRemoveLastCat() {
+    return cats.pop();
+}
+
+function destructivelyRemoveFirstCat() {
+    return cats.shift();
+}
+
+function appendCat(name) {
+    const newCats = [...cats];
+    newCats.push(name);
+    return newCats;
+}
+
+function prependCat(name) {
+    const newCats = [...cats];
+    newCats.unshift(name);
+    return newCats;
+}
+
+function removeFirstCat () {
+    const newCats = [...cats];
+    newCats.shift();
+    return newCats;
+}
+
+function removeLastCat() {
+    const newCats = [...cats];
+    newCats.pop();
+    return newCats;
+}
